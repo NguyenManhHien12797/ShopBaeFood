91c4ad270e1dd6329de95b22d69d7b8adcfd90b5
NguyenManhHien12797 <nguyenhien81f@gmail.com>
2022-11-02 10:21:40 +0700

    first commit

---
+++ src/main/java/com/example/trua_nay_an_gi/TruaNayAnGiApplication.java
@@ -0,0 +1,13 @@
+package com.example.trua_nay_an_gi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruaNayAnGiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruaNayAnGiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trua_nay_an_gi/TruaNayAnGiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trua_nay_an_gi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruaNayAnGiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
