6df06b1e320a57bcbd513a668c75a70d02d3661e
MinhHoang3011 <minhhoang301198@gmail.com>
2022-11-06 19:07:54 +0700

    model coupon

--- src/main/java/com/example/trua_nay_an_gi/model/product/Coupon.java
+++ src/main/java/com/example/trua_nay_an_gi/model/product/Coupon.java
@@ -2,18 +2,20 @@ package com.example.trua_nay_an_gi.model.product;
 
 import com.example.trua_nay_an_gi.model.app_users.MerchantCouponMap;
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
-@Getter
-@Setter
-@RequiredArgsConstructor
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Coupon {
+
+    public static final String FLAT = "FLAT";
+    public static final String PERCENT = "PERCENT";
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
