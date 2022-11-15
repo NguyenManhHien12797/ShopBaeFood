03f4486439abf72e19fc4d4ecd9f89592abb5343
LENOVO <imbakito1604@gmail.com>
2022-11-15 16:46:01 +0700

    hoan thien crud

--- src/main/java/com/example/trua_nay_an_gi/controller/OderDetailController.java
+++ src/main/java/com/example/trua_nay_an_gi/controller/OderDetailController.java
@@ -1,5 +1,6 @@
 package com.example.trua_nay_an_gi.controller;
 
+import com.example.trua_nay_an_gi.model.product.Coupon;
 import com.example.trua_nay_an_gi.model.product.Order;
 import com.example.trua_nay_an_gi.model.product.OrderDetail;
 import com.example.trua_nay_an_gi.service.oder_detail.OderDetailService;
@@ -30,5 +31,30 @@ public class OderDetailController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(orderDetailOptional.get(),HttpStatus.OK);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<OrderDetail> saveOrderdetail(@RequestBody  OrderDetail orderDetail) {
+        return new ResponseEntity<>(oderDetailService.save(orderDetail),HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OrderDetail> updateOrderdetail (@RequestBody OrderDetail orderDetail, @PathVariable Long id ) {
+        Optional<OrderDetail> orderDetails = oderDetailService.findById(id);
+        if (!orderDetails.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        orderDetail.setId(orderDetails.get().getId());
+        return new ResponseEntity<>(oderDetailService.save(orderDetail),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<OrderDetail> deleteOderdetail (@PathVariable Long id){
+        Optional<OrderDetail> orderDetailOptional = oderDetailService.findById(id);
+        if (!orderDetailOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        oderDetailService.remove(id);
+        return new ResponseEntity<>(orderDetailOptional.get(),HttpStatus.NO_CONTENT);
     }
  }
