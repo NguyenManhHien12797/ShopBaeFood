0a1db9fab87a51e50c29b13cdd9def2e9b8a0202
NguyenManhHien12797 <nguyenhien81f@gmail.com>
2022-11-16 11:50:24 +0700

    done cart tam thoi

--- src/main/java/com/example/trua_nay_an_gi/model/dto/CartDTO.java
+++ src/main/java/com/example/trua_nay_an_gi/model/dto/CartDTO.java
@@ -8,13 +8,13 @@ import lombok.Setter;
 @Getter
 @Setter
 @RequiredArgsConstructor
-public class CartDTO {
-    private Long id;
+public class CartDTO {   private Long id;
     private int quantity;
     private  double price;
     private Long user_id;
     private Long product_id;
     private double totalPrice;
+
 
 
 }
